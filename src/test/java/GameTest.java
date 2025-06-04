1a772d1d8ed11f815a2dacac677522d2c1d42946
nohog94 <nohog94@gmail.com>
2025-06-04 13:39:03 +0900

    [feature] 24

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class GameTest {
+    @Test
+    void createGame() {
+        Game game = new Game();
+        assertNotNull(game);
+    }
+
+    @Test
+    void throwExceptionWhenInputisNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Game game = new Game();
+            game.guess(null);
+        });
+    }
 }
