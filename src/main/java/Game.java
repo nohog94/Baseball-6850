bacc21f3e3f7ea439bd042ff27cc465ceb1fd797
clock <clockim@naver.com>
2025-06-04 14:57:12 +0900

    After Refactor

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,14 @@
 public class Game {
+    public void guess(String guessNumber) {
+        if ( guessNumber == null){
+            throw new IllegalArgumentException("Input cannot be null");
+        }
+        if(guessNumber.length() != 3){
+            throw new IllegalArgumentException("Input length must be 3");
+        }
+
+        for(char number : guessNumber.toCharArray()){
+            if(number < '0' || number > '9');
+        }
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,38 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GameTest {
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
+    private Game game;
+
+    @Test
+    void createGame() {
+        assertNotNull(game);
+    }
+
+    @Test
+    private void assertillegalArgument(String guessNumber) {
+        try {
+            game.guess(guessNumber);
+            fail();
+        } catch (IllegalArgumentException e) {
+
+        }
+    }
+
+    @Test
+    public void throwIllegalArgumentExceptionWhenInputIsNull() {
+        assertillegalArgument(null);
+        assertillegalArgument("12");
+        assertillegalArgument("1234");
+        assertillegalArgument("12s");
+    }
+
+
 }
