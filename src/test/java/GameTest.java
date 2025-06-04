e826982481c54b2867187f5cbeb1a7ac2c32768a
nohog94 <nohog94@gmail.com>
2025-06-04 13:40:56 +0900

    [refactoring] 26

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,19 +1,26 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GameTest {
+
+    private static Game game;
+
+    @BeforeAll
+    static void beforeAll() {
+        game = new Game();
+    }
+
     @Test
     void createGame() {
-        Game game = new Game();
         assertNotNull(game);
     }
 
     @Test
     void throwExceptionWhenInputisNull() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
             game.guess(null);
         });
     }
